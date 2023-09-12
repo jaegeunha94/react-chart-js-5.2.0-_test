f6aca8f3536b67758b9b4c61ac4426f91ed394a3
jaegeunha94 <46130542+jaegeunha94@users.noreply.github.com>
2023-09-12 14:06:11 +0900

    2 files modified

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './global.css';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
